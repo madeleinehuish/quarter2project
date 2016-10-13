8d0ea647506e58621fe4e696e7c2809b0456ad9a
madeleinehuish <vasistha@hotmail.com>
2016-10-12 20:36:58 -0700

    modified package.json, updated routes/users, migrations/users, seeds/users

--- migrations/20161012192501_users.js
+++ migrations/20161012192501_users.js
@@ -1,8 +1,18 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('first_name').notNullable().defaultTo('');
+    table.string('last_name').notNullable().defaultTo('');
+    table.string('email').notNullable().unique();
+    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable('users', (table) => {
+    table.dropColumn('hashed_password');
+  });
 };
