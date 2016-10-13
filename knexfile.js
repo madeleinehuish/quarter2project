33f3b2c424c04a60e3ce5e0b5a9b90cf5a4cda52
madeleinehuish <vasistha@hotmail.com>
2016-10-12 20:06:19 -0700

    1st update

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/movies_dev'
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/movies_test'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {min: 2, max: 7}
+  }
+};
