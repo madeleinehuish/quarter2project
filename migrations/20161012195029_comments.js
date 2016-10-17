3b77c749f6e023ce07b9c0193c6f8d53ccfea3d0
PortableTomb <tenderyoungpony@gmail.com>
2016-10-17 10:51:41 -0700

    added favorites & comments migrations and seeds

--- migrations/20161012195029_comments.js
+++ migrations/20161012195029_comments.js
@@ -1,8 +1,25 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('comments', (table) => {
+    table.increments();
+    table.integer('movies_id')
+         .notNullable()
+         .references('id')
+         .inTable('movies')
+         .onDelete('CASCADE')
+         .index();
+    table.integer('user_id')
+         .notNullable()
+         .references('id')
+         .inTable('users')
+         .onDelete('CASCADE')
+         .index();
+    table.text('comment')
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable('comments');
 };
---
+++ seeds/4_comments.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.seed = function(knex) {
+  return knex('comments').del()
+    .then(() => {
+      return knex('comments').insert([{
+        id: 1,
+        movies_id: 1,
+        user_id: 1,
+        comment: 'this was god awful!',
+        created_at: new Date('2016-10-16 14:26:16 UTC'),
+        updated_at: new Date('2016-10-16 14:26:16 UTC')
+      }]);
+    })
+    .then(() => {
+      return knex.raw(
+        "SELECT setval('comments_id_seq', (SELECT MAX(id) FROM comments));"
+      );
+    });
+};
