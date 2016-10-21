6c268c5fb894091c734b7deffeef9980f925d8cc
PortableTomb <tenderyoungpony@gmail.com>
2016-10-21 08:56:02 -0700

    saving my changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {
 
-// $('.scream').click(function() {
   var obj = document.createElement("audio");
-        obj.src="assets/scream.mp3";
+        obj.src="assets/jlcurtis-scream.mp3";
         obj.volume=0.50;
         obj.autoPlay=false;
         obj.preLoad=true;
         $(".scream").click(function() {
             obj.play();
         });
-  // new Audio('assets/scream.mp3').play();          //play corresponding audio
-// });
 
 })
