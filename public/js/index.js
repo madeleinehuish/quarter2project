31665517f57321423f71bd4b903900f179d4069b
madeleinehuish <vasistha@hotmail.com>
2016-10-20 22:01:12 -0700

    added better scream

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 // $('.scream').click(function() {
   var obj = document.createElement("audio");
-        obj.src="assets/scream.mp3";
+        obj.src="assets/jlcurtis-scream.mp3";
         obj.volume=0.50;
         obj.autoPlay=false;
         obj.preLoad=true;
