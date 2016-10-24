f560b27f70f1c0c0b773c7e50cce2f192914fa84
madeleinehuish <vasistha@hotmail.com>
2016-10-24 09:03:10 -0700

    Prepare the Heroku

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
   var obj = document.createElement("audio");
         obj.src="assets/jlcurtis-scream.mp3";
-        obj.volume=0.50;
+        obj.volume=0.80;
         obj.autoPlay=false;
         obj.preLoad=true;
         $(".scream").click(function() {
